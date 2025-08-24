6795424ad58158aa4c71a89323da1406c1342578
WiL <wilfridus.it@gmail.com>
2025-08-24 19:44:39 +0800

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Chandelier Kernel By Pavelc
+kernel.string=Templar Kernel for android12-5.10 devices by WiL (@Steambot12)
 do.devicecheck=1
 do.modules=0
 do.systemless=0
