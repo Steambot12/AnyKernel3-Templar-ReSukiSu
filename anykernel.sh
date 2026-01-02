087923b8cce2c9c4bf6c130d85a725ef839fe94d
WiL <wilfridus.it@gmail.com>
2026-01-03 02:21:09 +0800

    Update anykernel.sh
    Add Clean & Backup old config

--- anykernel.sh
+++ anykernel.sh
@@ -29,6 +29,27 @@ patch_vbmeta_flag=auto
 . tools/ak3-core.sh
 
 ui_print " "
+ui_print "- Cleaning old kernel artifacts..."
+
+# Backup and clean vendor modules
+if [ -d /vendor/lib/modules ]; then
+    mv /vendor/lib/modules /vendor/lib/modules.old 2>/dev/null
+    mkdir -p /vendor/lib/modules
+fi
+
+# Backup and clean system modules
+if [ -d /system/lib/modules ]; then
+    mv /system/lib/modules /system/lib/modules.old 2>/dev/null
+    mkdir -p /system/lib/modules
+fi
+
+# Clean module configs and cache
+rm -f /vendor/etc/modules.load 2>/dev/null
+rm -f /vendor/etc/modules.blocklist 2>/dev/null
+rm -f /data/vendor/modules/modules.dep 2>/dev/null
+rm -f /data/vendor/modules/modules.alias 2>/dev/null
+
+ui_print " "
 ui_print "- Checking kernel version..."
 
 current_kernel=$(uname -r | sed -E 's/^([0-9]+\.[0-9]+).*/\1/') 
@@ -52,3 +73,8 @@ ui_print "- $(strings "${home}"/Image 2>/dev/null | grep -E -m1 'Linux version.*
 flash_boot # Use flash_boot to skip ramdisk repack, e.g., for devices with init_boot ramdisk
 
 ## End boot install
+
+ui_print " "
+ui_print "- Installation complete!"
+ui_print "- Old modules backed up to .old folders"
+ui_print " "
